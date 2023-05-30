ab701e48274d4bacdddad25cf42d86154bb409d8
arraysArrais <joaopedroarrais@gmail.com>
2023-05-29 23:42:10 -0300

    fix: user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nickname', 45);
+            $table->string('first_name', 45);
+            $table->string('last_name', 45);
+            $table->string('profile_picture');
             $table->timestamps();
         });
     }
