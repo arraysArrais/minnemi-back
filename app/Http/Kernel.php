501e1c0b6155d187be52abbdc83a8fe42ce784aa
arraysArrais <joaopedroarrais@gmail.com>
2023-06-08 13:58:12 -0300

    create HeaderEnforcer middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\HeaderEnforcer::class, //prevent request of being sent without 'Accept' header, which may result in a non json response
     ];
 
     /**
