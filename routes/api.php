b7dc95a5388ca7d8150c5864a5507a84b5308028
arraysArrais <joaopedroarrais@gmail.com>
2023-06-08 13:27:02 -0300

    add auth routes

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware(SetLocale::class)->group(function (): void {
     Route::post('/letter', [LetterController::class, 'create']);
 });
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('verify', [AuthController::class, 'verify']);
+    Route::post('revoke', [AuthController::class, 'revokeToken']);
+});
+
+//protected api routes
+Route::middleware(['auth:api'])->group(function () {
+    
+});
