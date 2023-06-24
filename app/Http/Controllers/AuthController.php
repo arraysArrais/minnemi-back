f73d5881dec42ffed8a395a1d23ccc36ffdf7731
arraysArrais <joaopedroarrais@gmail.com>
2023-06-24 15:51:57 -0300

    :books: docs: add logout endpoint annotation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *         required=true,
      *         @OA\JsonContent(
      *      type="object",
-     *      @OA\Property(property="email", type="string", example="joaopedroarrais@gmail.com"),
+     *      @OA\Property(property="email", type="string", example="teste@teste.com"),
      *      @OA\Property(property="password", type="string", example="123456"),
      *   ),
      * ),
@@ -43,7 +43,7 @@ class AuthController extends Controller
      *       ),
      *     ),
      *     @OA\Response(
-     *         response=403,
+     *         response=401,
      *         description="Incorrect username or password",
      *     @OA\JsonContent(
      *        type="object",
@@ -80,9 +80,34 @@ class AuthController extends Controller
     }
 
     /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     operationId="logout",
+     *     security={{"bearerAuth": {}}},
+     *     tags={"auth"},
+     *     summary="logout",
+     *     description="Logout endpoint",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *         type="object",
+     *          @OA\Property(property="message", type="string", example="Successfully logged out"),
+     *       ),
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Incorrect token",
+     *     @OA\JsonContent(
+     *        type="object",
+     *         @OA\Property(property="error", type="string", example="Unauthorized."),
+     *      ),
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error while fetching data in database"
+     *     ),
+     * ),
      */
     public function logout()
     {
