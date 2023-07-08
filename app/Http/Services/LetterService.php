e9dc8ebc74476595877debb3064098c0b7444cea
arraysArrais <joaopedroarrais@gmail.com>
2023-07-08 10:53:43 -0300

    :sparkles: feat: create dispatch method

--- app/Http/Services/LetterService.php
+++ app/Http/Services/LetterService.php
@@ -4,9 +4,15 @@ namespace App\Http\Services;
 
 use App\Http\Requests\LetterRequest;
 use App\Models\Letter;
+use Carbon\Carbon;
 
 class LetterService{
     public function InsertLetter(LetterRequest $r){
         return Letter::create($r->toArray());
+    }
+
+    public function DispatchLetter(){
+        $letters = Letter::all()->where('date_to_send', Carbon::now()->format('Y-m-d'));
+        return $letters;
     }
 }
